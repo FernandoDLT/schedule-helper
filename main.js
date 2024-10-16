d20048f2be4579c6e9bb446f243ba75233d0ce1b
FERNANDO DE LA TORRE <96254499+FernandoDLT@users.noreply.github.com>
2024-10-15 18:16:45 -0600

    Updated nav for mobile

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const carousel = document.querySelector("#carousel");
-  const carouselItems = document.querySelectorAll("#carousel li");
-  const prevButton = document.querySelector(".prev");
-  const nextButton = document.querySelector(".next");
+const carousel = document.querySelector("#carousel");
+const carouselItems = document.querySelectorAll("#carousel li");
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+  
+const burgerMenu = document.querySelector('.burger-menu');
+const navContainer = document.querySelector('.nav-container');
+
+burgerMenu.addEventListener('click', () => {
+   navContainer.classList.toggle('nav-open');
+});
+
+
+// const burgerMenu = document.querySelector('.burger-menu');
+// const nav = document.querySelector('.nav-container nav');
+
+// burgerMenu.addEventListener('click', () => {
+//    nav.classList.toggle('active');  // Toggles the "active" class to show/hide the menu
+// });
   
   let currentIndex = 0;
   const totalItems = carouselItems.length;
